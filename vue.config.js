80060eea50b5e6dacb35f835baf56dd6dfbde582
dmmsilch <dmitry.masyuto@gmail.com>
2023-04-22 13:02:00 +0300

    deploy editing

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/first-vue-project/',
+  // publicPath: '/first-vue-project/',
 });
